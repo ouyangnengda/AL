e52813ca6f4ed2abe1bae8f2742f2fa5cfe48b3d
ouyangnengda <42287049+ouyangnengda@users.noreply.github.com>
2019-09-02 15:18:21 +0800

    腾讯面试题：宝箱和钥匙

---
+++ Interview/src/main/java/TX/BoxAndKey.java
@@ -0,0 +1,62 @@
+package TX;
+
+import java.util.Scanner;
+
+/**
+ * @Description: 宝箱和钥匙。每一个宝箱上有一个数字，每一个钥匙上有一个整数，
+ * 只有宝箱上的数字与钥匙上的数字相加之和为奇数这把钥匙才能打开宝箱，而且每个钥匙只能使用一次。
+ * 第一行输入两个数字中间用空格隔开，第一个数字表示宝箱的数量，第二个数字表示钥匙的数量。
+ * 接下来每一行都是这样的格式，求能打开宝箱的最大数目。
+ * @Author: 欧阳能达
+ * @Created: 2019年09月02日 15:01:00
+ **/
+public class BoxAndKey {
+    public static void main(String[] args) {
+        test();
+    }
+
+
+    private static void test() {
+        Scanner input = new Scanner(System.in);
+        int sumA = input.nextInt();
+        int sumB = input.nextInt();
+
+        int[] a = new int[sumA];
+        for (int i = 0; i < sumA; i++) {
+            a[i] = input.nextInt();
+        }
+
+        int[] b = new int[sumB];
+        for (int i = 0; i < sumB; i++) {
+            b[i] = input.nextInt();
+        }
+
+        System.out.println(boxAndKey(a, b));
+    }
+
+    private static int boxAndKey(int[] a, int[] b) {
+        //没用过的钥匙为0，用过的钥匙为1
+        int[] bit = new int[b.length];
+        for (int i = 0; i < bit.length; i++) {
+            bit[i] = 0;
+        }
+
+        //表示可打开的宝箱总数
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+
+
+            for (int j = 0; j < b.length; j++) {
+                //如果i宝箱和j钥匙相加为奇数 && 钥匙没用过
+                if ((a[i]+b[j])%2 == 1 && bit[j] == 0) {
+                    bit[j] = 1;
+                    sum = sum + 1;
+                    break;
+                }
+            }
+        }
+
+        return sum;
+
+    }
+}
