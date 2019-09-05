d41140eecc44bc451d4a827602caae68afaf30ab
ouyangnengda <42287049+ouyangnengda@users.noreply.github.com>
2019-09-05 23:15:56 +0800

    算法

---
+++ Leetcode/src/main/java/Leetcode_1_TwoSum.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description:
+ * @Author: 欧阳能达
+ * @Created: 2019年09月05日 22:59:00
+ **/
+public class Leetcode_1_TwoSum {
+    public static void main(String[] args) throws IllegalAccessException {
+        for (int i : twoSum(new int[] {1, 1}, 2)) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target){
+        if (nums == null || nums.length == 1) {
+            return nums;
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int i;
+        for (i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return nums;
+    }
+}
